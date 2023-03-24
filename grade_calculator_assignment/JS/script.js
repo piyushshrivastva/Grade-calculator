888c36b8a50e681768923fe9203905165d87763c
piyushshrivastva <122368417+piyushshrivastva@users.noreply.github.com>
2023-03-24 18:37:42 +0530

    Add files via upload

---
+++ grade_calculator_assignment/JS/script.js
@@ -0,0 +1,7 @@
+let engscore = document.getElementById("english")
+let matscore = document.getElementById("maths")
+let physcore = document.getElementById("physics")
+let chemscore = document.getElementById("chemistry")
+let comscore = document.getElementById("computer")
+
+let calculategrade = document.getElementById("calgrade")
